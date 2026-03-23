26ee86be9a1e8559db17d7620d61364513329ebf
David Telles <you@example.com>
2026-03-23 15:32:26 -0300

    tentando enviar dados via API

--- app.js
+++ app.js
@@ -15,8 +15,15 @@ app.get('/ver-dados', async (req, res) => {
 
 app.post('/enviar-dados', async (req, res) => {
     try {
-        const result = await mysql.execute('INSERT INTO dados (nome_corrida, nome_equipe, total_voltas, tempo_ultima_volta, velocidade_media, posicao) VALUES ("Silverstone", "Ferrari", 50, "00:01:32.450", 180.5, 1), ("Silverstone", "McLaren", 50, "00:01:33.210", 178.2, 2);')
-        return req.status(200).json(result);
+        const result = await mysql.execute(`
+            INSERT INTO dados 
+            (nome_corrida, nome_equipe, total_voltas, tempo_ultima_volta, velocidade_media, posicao) 
+            VALUES 
+            ("Silverstone", "Ferrari", 50, "00:01:32.450", 180.5, 1), 
+            ("Silverstone", "McLaren", 50, "00:01:33.210", 178.2, 2);
+        `);
+
+        return res.status(200).json(result);
     } catch (error) {
         return res.status(500).json({
             error: error.message
